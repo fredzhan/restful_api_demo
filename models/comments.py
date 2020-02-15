053a4a045d0b0ef1c39ff91aa13ab343af3a80ec
Fred Zhan <frdzhan_@163.com>
2020-02-15 14:20:00 +0800

    Initial version of RESTFul.

---
+++ models/comments.py
@@ -0,0 +1,20 @@
+# import sys
+# import os
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+from flask import g
+
+class CommentsModel:
+    def __init__(self):
+        pass
+
+    def searchComments(self, keyword):
+        QUERY_SQL = '''
+            SELECT * FROM comments WHERE content LIKE '%{kw}%';
+        '''
+        records = g.db.query(QUERY_SQL.format(kw=keyword)).fetchall()
+        result = {'data':[]}
+        for r in records:
+            r_dict = {}
+            r_dict['id'], r_dict['content'], r_dict['creation_time'], r_dict['score'], r_dict['nickname'] =  r
+            result['data'].append(r_dict)
+        return result
